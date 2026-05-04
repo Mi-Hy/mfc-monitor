4cd8f024f002c3df92c75144d17bf99d2f67c68f
Jarne Van Mulders <33618569+jarnevanmulders@users.noreply.github.com>
2026-05-04 16:12:53 +0200

    Create test.py

---
+++ controller/test.py
@@ -0,0 +1,60 @@
+from lib.ep_handler import *
+from lib.influxdb import *
+from lib.tca9554 import TCA9554
+from lib.configuration import *
+
+io = TCA9554()
+
+CELLS = 2
+
+value_mapper = {0: 3, 1: 2, 2: 1, 3: 0, 4: 7, 5: 6, 6: 5, 7: 4}
+
+if __name__ == "__main__":
+
+    # Get config file
+    config = retrieve_yaml_file()
+
+    while(config['status']['load'] == 1):
+        #   Debug message
+        print("[Voltage] ⏳ Waiting for load test to complete...")
+        
+        #   Wait 5 seconds before checking again
+        time.sleep(5)
+
+        #   Update config file to indicate voltage test is running
+        config = retrieve_yaml_file()
+
+    update_yaml_flag("status", "voltage", 1)
+
+    ep_change_resistance(0)
+
+    time.sleep(3)
+
+    # while True:
+
+    voltage_buffer = []
+
+    for i in range(CELLS):
+        io.set_output(value_mapper.get(i, i), 1)
+
+        time.sleep(1.5)
+
+        ep_data = get_ep_data()
+
+        # print(ep_data)
+
+        print(f"[Cell {i}] Buffer Voltage: {ep_data['buffer_voltage_mv']} mV")
+
+        voltage_buffer.append(ep_data["buffer_voltage_mv"])
+
+        time.sleep(30)
+
+        io.set_output(value_mapper.get(i, i), 0)
+
+        time.sleep(2)
+    
+    voltage_data = {f"cell_{i}": voltage_buffer[i] for i in range(CELLS)}
+
+    send_voltage_info(config, "[Voltage]", voltage_data)
+
+    update_yaml_flag("status", "voltage", 0)
